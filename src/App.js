13e12be6d822867bc5ef987dabf6226528115d05
rahul angural <rangg5620@gmail.com>
2019-08-17 18:16:31 -0500

    todoinput structured

--- src/App.js
+++ src/App.js
@@ -33,20 +33,21 @@ class App extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="col-10 mx-auto col-md-8 mt-5" />
-          <h3 className="text-capitalize text-center">todo input</h3>
-          <TodoInput
-            item={this.state.item}
-            handleChange={this.handleChange}
-            handleSubmit={this.handleSubmit}
-            editItem={this.state.editItem}
-          />
-          <TodoList
-            items={this.state.items}
-            clearList={this.clearList}
-            handleDelete={this.handleDelete}
-            handleEdit={this.handleEdit}
-          />
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">todo input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
         </div>
       </div>
     );
